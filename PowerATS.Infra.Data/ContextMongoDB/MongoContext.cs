01ae148282a9ade41c090fee45e6ee616e506a26
Thiago Ladeira <tiladeira@hotmail.com>
2024-01-29 14:38:36 -0300

    Falta no ajuste do context

--- PowerATS.Infra.Data/ContextMongoDB/MongoContext.cs
+++ PowerATS.Infra.Data/ContextMongoDB/MongoContext.cs
@@ -12,12 +12,19 @@ namespace PowerATS.Infra.Data.ContextMongoDB
         }
 
         public DbSet<Vaga> Vagas { get; init; }
+        public DbSet<Candidato> Candidatos { get; init; }
+        public DbSet<CandidatoVaga> CandidatosVagas { get; init; }
+        public DbSet<Curriculo> Curriculos { get; init; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Vaga>();
+            modelBuilder.Entity<Candidato>();
+            modelBuilder.Entity<CandidatoVaga>();
+            modelBuilder.Entity<Curriculo>();
         }
     }
 }
